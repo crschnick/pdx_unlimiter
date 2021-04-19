0d2021fc419d0d7d1ba39efece83b1831df3447d
Christopher Schnick <c.schnick@monospark.org>
2021-04-19 13:48:44 +0200

    Add missing rakaly warning

--- app/src/main/java/com/crschnick/pdx_unlimiter/app/util/integration/RakalyHelper.java
+++ app/src/main/java/com/crschnick/pdx_unlimiter/app/util/integration/RakalyHelper.java
@@ -1,6 +1,8 @@
 package com.crschnick.pdx_unlimiter.app.util.integration;
 
 import com.crschnick.pdx_unlimiter.app.core.PdxuInstallation;
+import com.crschnick.pdx_unlimiter.app.gui.dialog.GuiDialogHelper;
+import javafx.scene.control.Alert;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 
@@ -10,7 +12,23 @@ import java.nio.file.Path;
 
 public class RakalyHelper {
 
+    private static void showError() {
+        GuiDialogHelper.showBlockingAlert(alert -> {
+            alert.setAlertType(Alert.AlertType.ERROR);
+            alert.setTitle("Rakaly error");
+            alert.setHeaderText("It seems like a core component, the Rakaly Ironman converter, is missing.\n" +
+                   "This is usually caused by a not fully completed update.\n" +
+                   "Please try restarting the Pdx-Unlimiter while you are connected to the internet to fix this.\n\n" +
+                   "If this doesn't help, please report this error to the developers.");
+        });
+    }
+
     public static Path meltSavegame(Path file) throws Exception {
+        if (!Files.exists(PdxuInstallation.getInstance().getRakalyExecutable())) {
+            showError();
+            throw new IOException("Rakaly melter missing");
+        }
+
         var proc = new ProcessBuilder(
                 PdxuInstallation.getInstance().getRakalyExecutable().toString(),
                 "melt",
