88424fe25943f775ccdb4467ef41672302f49fad
Christopher Schnick <st141923@stud.uni-stuttgart.de>
2022-10-16 14:13:53 +0200

    Add rendering hints

--- pdxu-app/src/main/java/com/crschnick/pdxu/app/gui/game/Vic3TagRenderer.java
+++ pdxu-app/src/main/java/com/crschnick/pdxu/app/gui/game/Vic3TagRenderer.java
@@ -15,7 +15,24 @@ public class Vic3TagRenderer {
         }
 
         BufferedImage i = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        Graphics g = i.getGraphics();
+        Graphics2D g = (Graphics2D) i.getGraphics();
+
+        g.setRenderingHint(
+                RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_BILINEAR
+        );
+        g.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON
+        );
+        g.setRenderingHint(
+                RenderingHints.KEY_RENDERING,
+                RenderingHints.VALUE_RENDER_QUALITY
+        );
+        g.setRenderingHint(
+                RenderingHints.KEY_STROKE_CONTROL,
+                RenderingHints.VALUE_STROKE_PURE
+        );
 
         for (var sub : coa.getSubs()) {
             var rawPatternImg = CoatOfArmsRenderer.VIC3.pattern(g, sub, ctx, width, height);
