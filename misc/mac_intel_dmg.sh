007a41de06b6c9c7ed3186a200fbbbd9cd4bfa87
Christopher Schnick <st141923@stud.uni-stuttgart.de>
2022-10-20 21:36:18 +0200

    Add mac dmg build

---
+++ misc/mac_intel_dmg.sh
@@ -0,0 +1,3 @@
+./create-dmg \
+--volname "Pdx-Unlimiter" --volicon "misc/logo.icns" --window-pos 200 120 --window-size 512 228 --icon-size 128 --icon "Pdx-Unlimiter.app" 64 50 \
+--app-drop-link 320 50 "build/pdx_unlimiter-mac_intel.dmg" "build/dist/Pdx-Unlimiter.app"
