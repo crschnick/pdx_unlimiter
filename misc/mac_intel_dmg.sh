e8b67b3fc97f6c579ece9b0c1796054ccf4aa22b
Christopher Schnick <st141923@stud.uni-stuttgart.de>
2022-10-20 21:50:29 +0200

    Update mac_intel_dmg.sh

--- misc/mac_intel_dmg.sh
+++ misc/mac_intel_dmg.sh
@@ -1,3 +1,17 @@
+#!/bin/sh
+
+BASE=$(dirname "$0")
+
+mkdir -p "$BASE/../build/dist/"
+rm "$BASE/../build/dist/pdx_unlimiter-mac_intel.dmg"
+
 ./create-dmg \
---volname "Pdx-Unlimiter" --volicon "misc/logo.icns" --window-pos 200 120 --window-size 512 228 --icon-size 128 --icon "Pdx-Unlimiter.app" 64 50 \
---app-drop-link 320 50 "build/pdx_unlimiter-mac_intel.dmg" "build/dist/Pdx-Unlimiter.app"
+  --volname "Pdx-Unlimiter" \
+  --volicon "$BASE/logo.icns" \
+  --window-pos 200 120 \
+  --window-size 512 228 \
+  --icon-size 128 \
+  --icon "Pdx-Unlimiter.app" 64 50 \
+  --app-drop-link 320 50 \
+  "$BASE/../pdx_unlimiter-mac_intel.dmg" \
+  "$BASE/../build/dist/Pdx-Unlimiter.app"
