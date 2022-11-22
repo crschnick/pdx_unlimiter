40691a1e07afdd40dcc2b9b3a2e1a1576f7ec035
Christopher Schnick <st141923@stud.uni-stuttgart.de>
2022-11-22 07:52:00 +0100

    Fixes for GraalVM javascript engine in production

--- pdxu-io/src/main/java/module-info.java
+++ pdxu-io/src/main/java/module-info.java
@@ -4,7 +4,6 @@ module com.crschnick.pdxu.io {
     exports com.crschnick.pdxu.io.node;
 
     requires org.apache.commons.lang;
-    requires org.graalvm.sdk;
-    requires org.graalvm.truffle;
+    requires org.graalvm.js;
     requires static lombok;
 }
