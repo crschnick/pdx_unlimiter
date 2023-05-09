6edc1f5f46af65bc6e0c4ef89df4aaa40d678c22
Christopher Schnick <crschnick@xpipe.io>
2023-05-10 00:57:34 +0200

    Disable ck2 branches for ironman

--- pdxu-app/src/main/java/com/crschnick/pdxu/app/savegame/SavegameBranches.java
+++ pdxu-app/src/main/java/com/crschnick/pdxu/app/savegame/SavegameBranches.java
@@ -7,6 +7,11 @@ public class SavegameBranches {
     public static boolean supportsBranching(SavegameEntry<?,?> e) {
         var ctx = SavegameContext.getContext(e);
 
+        // We can't change the name of ironman save games here!
+        if (ctx.getGame() == Game.CK2) {
+            return !ctx.getInfo().getData().isIronman();
+        }
+
         // VIC2 does not support rewriting yet for non-ironman
         if (ctx.getGame() == Game.VIC2) {
             return ctx.getInfo().getData().isIronman();
