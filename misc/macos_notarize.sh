6e1d300245190b251c06ff1fc64c80af871a9a3e
Christopher Schnick <crschnick@xpipe.io>
2024-10-31 23:05:06 +0100

    Sign fix

--- misc/macos_notarize.sh
+++ misc/macos_notarize.sh
@@ -16,6 +16,8 @@ mvn clean compile package assembly:single
 cp target/SignPackage-1.0-jar-with-dependencies.jar ./SignPackage.jar
 java -jar "$TMPDIR/signpackage/SignPackage.jar" -r -d "$APP_DIR" -t -k "Developer ID Application: Christopher Schnick (PF6V9HYACS)" -e "$1/misc/Entitlements.plist"
 
+codesign -vvv --deep --entitlements "$1/misc/Entitlements.plist" --options=runtime --force --strict --sign "Developer ID Application: Christopher Schnick (PF6V9HYACS)" "$APP_DIR/Contents/MacOS/Pdx-Unlimiter"
+
 echo "Create keychain profile"
 xcrun notarytool store-credentials "notarytool-profile" --apple-id "$MAC_NOTARIZATION_APPLE_ID" --team-id "$MAC_NOTARIZATION_TEAM_ID" --password "$MAC_APP_SPECIFIC_PASSWORD"
 
