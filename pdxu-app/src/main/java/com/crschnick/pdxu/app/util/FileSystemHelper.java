32daf22b48f90cc8c14ce604fbf05b2a5b86aec2
Christopher Schnick <crschnick@xpipe.io>
2025-10-10 23:32:24 +0200

    Fix documents dir detection [release]

--- pdxu-app/src/main/java/com/crschnick/pdxu/app/util/FileSystemHelper.java
+++ pdxu-app/src/main/java/com/crschnick/pdxu/app/util/FileSystemHelper.java
@@ -2,16 +2,27 @@ package com.crschnick.pdxu.app.util;
 
 import com.crschnick.pdxu.app.core.AppSystemInfo;
 
+import javax.swing.filechooser.FileSystemView;
 import java.nio.file.Path;
 
 public class FileSystemHelper {
 
+    private static Path documentsPath;
+
     public static Path getUserDocumentsPath() {
-        return switch (OsType.ofLocal()) {
+        if (documentsPath != null) {
+            return documentsPath;
+        }
+
+        documentsPath = switch (OsType.ofLocal()) {
             case OsType.Linux linux -> AppSystemInfo.ofLinux().getUserHome().resolve(".local", "share");
             case OsType.MacOs macOs -> AppSystemInfo.ofMacOs().getUserHome().resolve("Library", "Application Support");
-            case OsType.Windows windows -> AppSystemInfo.ofWindows().getUserHome().resolve("Documents");
+            case OsType.Windows windows -> Path.of(WindowsRegistry.of().readStringValueIfPresent(WindowsRegistry.HKEY_CURRENT_USER,
+                    "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders", "Personal")
+                    .orElse(AppSystemInfo.ofCurrent().getUserHome().resolve("Documents").toString()));
         };
+
+        return documentsPath;
     }
 
     public static String getFileSystemCompatibleName(String name) {
