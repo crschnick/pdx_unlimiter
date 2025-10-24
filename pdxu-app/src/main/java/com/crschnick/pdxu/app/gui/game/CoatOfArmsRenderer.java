cee7e7f3943dd66f1dd80ba307bee60a2a24320f
Christopher Schnick <crschnick@xpipe.io>
2025-10-24 10:53:35 +0200

    Improve red and white mask blending

--- pdxu-app/src/main/java/com/crschnick/pdxu/app/gui/game/CoatOfArmsRenderer.java
+++ pdxu-app/src/main/java/com/crschnick/pdxu/app/gui/game/CoatOfArmsRenderer.java
@@ -188,7 +188,7 @@ public abstract class CoatOfArmsRenderer {
                         continue;
                     }
 
-                    var diff = maskIndex == 1 ? ColorHelper.colorDistance(maskRgb, PATTERN_COLOR_1) : maskIndex == 2 ?
+                    var diff = maskIndex == 1 ? ColorHelper.colorDistance(maskRgb & 0x00FFFF00, PATTERN_COLOR_1) : maskIndex == 2 ?
                             ColorHelper.colorDistance(maskRgb, PATTERN_COLOR_2) : ColorHelper.colorDistance(maskRgb, PATTERN_COLOR_3);
                     var t = Math.clamp(diff / 255.0, 0.0, 1.0);
 
