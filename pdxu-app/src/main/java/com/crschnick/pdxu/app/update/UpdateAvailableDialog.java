d7d792101c750211a5eba065b997a5ca67a2d1f2
Christopher Schnick <crschnick@xpipe.io>
2025-11-10 07:03:03 +0100

    prevent showing multiple dialogs

--- pdxu-app/src/main/java/com/crschnick/pdxu/app/update/UpdateAvailableDialog.java
+++ pdxu-app/src/main/java/com/crschnick/pdxu/app/update/UpdateAvailableDialog.java
@@ -4,10 +4,21 @@ import com.crschnick.pdxu.app.comp.Comp;
 import com.crschnick.pdxu.app.comp.base.MarkdownComp;
 import com.crschnick.pdxu.app.comp.base.ModalOverlay;
 import com.crschnick.pdxu.app.issue.TrackEvent;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
 
 public class UpdateAvailableDialog {
 
+    private static final Property<ModalOverlay> last = new SimpleObjectProperty<>();
+
     public static void showIfNeeded(boolean wait) {
+        var l = last.getValue();
+        if (l != null && l.isShowing()) {
+            return;
+        }
+
         UpdateHandler uh = AppDistributionType.get().getUpdateHandler();
         if (uh.getLastUpdateCheckResult().getValue() == null) {
             return;
@@ -33,6 +44,7 @@ public class UpdateAvailableDialog {
             modal.addButton(action);
         }
 
+        last.setValue(modal);
         if (wait) {
             modal.showAndWait();
         } else {
