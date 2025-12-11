bb77308f16cb57d501fd82ab96ed7be32f1708ef
Christopher Schnick <crschnick@xpipe.io>
2025-12-11 14:35:56 +0100

    Reenable save aot training

--- pdxu-app/src/main/java/com/crschnick/pdxu/app/core/AppAotTrain.java
+++ pdxu-app/src/main/java/com/crschnick/pdxu/app/core/AppAotTrain.java
@@ -2,7 +2,10 @@ package com.crschnick.pdxu.app.core;
 
 import com.crschnick.pdxu.app.core.mode.AppOperationMode;
 import com.crschnick.pdxu.app.util.OsType;
+import com.crschnick.pdxu.app.util.RakalyHelper;
 import com.crschnick.pdxu.app.util.ThreadHelper;
+import com.crschnick.pdxu.io.node.NodeEvaluator;
+import com.crschnick.pdxu.io.parser.TextFormatParser;
 
 public class AppAotTrain {
 
@@ -16,18 +19,17 @@ public class AppAotTrain {
             }
         }
 
-        // TODO: This crashes the JVM on some systems. Why?
-        //        if (OsType.ofLocal() == OsType.WINDOWS) {
-        //            var rootDir = AppInstallation.ofCurrent()
-        //                    .getBaseInstallationPath()
-        //                    .getParent()
-        //                    .getParent()
-        //                    .getParent()
-        //                    .getParent();
-        //            var save = rootDir.resolve("misc").resolve("train_save.eu5");
-        //            var bytes = RakalyHelper.toEquivalentPlaintext(save);
-        //            var parsed = TextFormatParser.eu5().parse("train_save", bytes, 0, true);
-        //            NodeEvaluator.evaluateArrayNode(parsed);
-        //        }
+        if (OsType.ofLocal() == OsType.WINDOWS) {
+            var rootDir = AppInstallation.ofCurrent()
+                    .getBaseInstallationPath()
+                    .getParent()
+                    .getParent()
+                    .getParent()
+                    .getParent();
+            var save = rootDir.resolve("misc").resolve("train_save.eu5");
+            var bytes = RakalyHelper.toEquivalentPlaintext(save);
+            var parsed = TextFormatParser.eu5().parse("train_save", bytes, 0, true);
+            NodeEvaluator.evaluateArrayNode(parsed);
+        }
     }
 }
