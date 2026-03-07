5f02b143561701caf0733866d170391a91c6b9e6
Christopher Schnick <crschnick@xpipe.io>
2026-03-07 19:28:33 +0100

    Fix theme transitions

--- pdxu-app/src/main/java/com/crschnick/pdxu/app/core/AppTheme.java
+++ pdxu-app/src/main/java/com/crschnick/pdxu/app/core/AppTheme.java
@@ -261,9 +261,10 @@ public class AppTheme {
             var scene = stage.getScene();
             Pane root = (Pane) scene.getRoot();
             Image snapshot = scene.snapshot(null);
+
+            newTheme.apply();
             ImageView imageView = new ImageView(snapshot);
             root.getChildren().add(imageView);
-            newTheme.apply();
 
             Platform.runLater(() -> {
                 // Animate!
@@ -278,6 +279,13 @@ public class AppTheme {
                     root.getChildren().remove(imageView);
                 });
                 transition.play();
+            });
+            
+            // Force mark dirty the root node
+            // This ensures that the stylesheets are applied properly to the nodes in case no layout change is done
+            root.getStyleClass().add("theme-update-notification");
+            Platform.runLater(() -> {
+                root.getStyleClass().remove("theme-update-notification");
             });
         });
     }
