44a6ada84715334ba94238c17af67804d79252a4
Christopher Schnick <crschnick@xpipe.io>
2026-06-18 11:47:31 +0200

    Port fixes from kickstartfx

--- dist/msi/build_installer_x86_64.bat
+++ dist/msi/build_installer_x86_64.bat
@@ -1,8 +1,15 @@
-CALL "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat" || CALL "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+@echo off
+
+for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set InstallDir=%%i
+)
+echo Found install dir at %InstallDir%
+
+call "%InstallDir%\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-msbuild "%~dp0\WindowsInstaller.sln" -t:Restore
-msbuild "%~dp0\WindowsInstaller.sln" -property:Configuration=Release
+"%InstallDir%\MSBuild\Current\Bin\msbuild.exe" "%~dp0\WindowsInstaller.sln" -t:Restore
+"%InstallDir%\MSBuild\Current\Bin\msbuild.exe" "%~dp0\WindowsInstaller.sln" -property:Configuration=Release -property:Platform=x64
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 copy "%~dp0\..\build\dist\installers\windows\Release\en-us\msi-installer.msi" "%~dp0\..\build\dist\artifacts\%ARTIFACT_NAME%-installer-windows-x86_64.msi"
